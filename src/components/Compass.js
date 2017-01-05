3b0d819c093e702f9edf8477278a1e4029c96938
Evan Gatchell <egatchell@changehealthcare.com>
2017-01-04 23:56:41 -0600

    more steps.

--- src/components/Compass.js
+++ src/components/Compass.js
@@ -6,10 +6,26 @@ class Compass extends React.Component {
   constructor(props){
       super()
       this.constructQuestions = this.constructQuestions.bind(this);
+      this.checkCorrectAnswer = this.checkCorrectAnswer.bind(this);
 
       this.state = {
         questions: content.questions
       }
+  }
+
+
+  checkCorrectAnswer(choice, question, index) {
+    if(choice === question.answer){
+      alert('correctomundo');
+    } else {
+      alert('incorrect');
+    }
+
+    const updatedQuestion = Object.assign(question, {completed: true });
+    const updatedQuestions = this.state.questions.filter(q => !q.completed).map((q,i) => index === i ? updatedQuestion : q);
+    this.setState({
+      questions: updatedQuestions
+    })
   }
 
   constructQuestions(questions) {
@@ -17,7 +33,18 @@ class Compass extends React.Component {
       return (
         <div className="question-block" key={i}>
           <h3>{q.title}</h3>
-          {q.completed.toString()}
+          <p>{q.content}</p>
+          <p>Completed? {q.completed.toString()}</p>
+
+          {q.options.map((o,i)=>{
+            return (
+              <div key={i}>
+                <button onClick={this.checkCorrectAnswer.bind(this, o, q, i)}>
+                {o}
+                </button>
+              </div>
+            )
+          })}
         </div>
       );
     });
