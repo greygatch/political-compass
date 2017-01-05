4d07f55b5abd1e5c32f81ad26434babfd7d40742
Evan Gatchell <egatchell@changehealthcare.com>
2017-01-05 16:04:15 -0600

    Reformat questions.

--- src/components/Compass.js
+++ src/components/Compass.js
@@ -6,20 +6,18 @@ class Compass extends React.Component {
   constructor(props){
       super()
       this.constructQuestions = this.constructQuestions.bind(this);
-      this.checkCorrectAnswer = this.checkCorrectAnswer.bind(this);
+      this.computeChoice = this.computeChoice.bind(this);
 
       this.state = {
-        questions: content.questions
+        questions: content.questions,
+        score: 0,
+        currentQuestion: 0
       }
   }
 
 
-  checkCorrectAnswer(choice, question, index) {
-    if(choice === question.answer){
-      alert('correctomundo');
-    } else {
-      alert('incorrect');
-    }
+  computeChoice(choice, question, index) {
+    const isCorrect = choice === question.answer ? true : false;
 
     const updatedQuestion = Object.assign(question, {completed: true });
     const updatedQuestions = this.state.questions.filter(q => !q.completed).map((q,i) => index === i ? updatedQuestion : q);
@@ -30,7 +28,9 @@ class Compass extends React.Component {
 
   constructQuestions(questions) {
     if(questions.length === 0){
-      return <h1> You lose, bitch. </h1>
+      return (
+        <h1> You lose, bitch. </h1>
+      );
     }
     return questions.map((q, i) => {
       return (
@@ -42,9 +42,7 @@ class Compass extends React.Component {
           {q.options.map((o,i)=>{
             return (
               <div key={i}>
-                <button onClick={this.checkCorrectAnswer.bind(this, o, q, i)}>
-                {o}
-                </button>
+                <button onClick={this.computeChoice.bind(this, o, q, i)}>{o}</button>
               </div>
             )
           })}
