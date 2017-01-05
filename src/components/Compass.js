9548d7b978ffffe220a35fba9709f790690dd0de
Evan Gatchell <egatchell@changehealthcare.com>
2017-01-05 16:54:38 -0600

    linting.

--- src/components/Compass.js
+++ src/components/Compass.js
@@ -1,27 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router';
 import * as content from '../../content/questions.json';
 
 class Compass extends React.Component {
-  constructor(props){
-      super()
-      this.constructQuestions = this.constructQuestion.bind(this);
-      this.computeChoice = this.computeChoice.bind(this);
-      this.resetApp = this.resetApp.bind(this);
+  constructor() {
+    super();
+    this.constructQuestions = this.constructQuestion.bind(this);
+    this.resetApp = this.resetApp.bind(this);
 
-      this.state = {
-        questions: content.questions,
-        questionIndex: 0,
-        socialScore: 0,
-        economicsScore: 0
-      }
+    this.state = {
+      questions: content.questions,
+      questionIndex: 0,
+      socialScore: 0,
+      economicsScore: 0
+    };
   }
 
 
   computeChoice(option, question) {
     const { socialScore, economicsScore } = this.state;
-    const completedQuestion = Object.assign(question, {completed: true });
-    const updatedQuestions = this.state.questions.map((q,i) => q.id === question.id ? completedQuestion : q);
+    const completedQuestion = Object.assign(question, { completed: true });
+    const updatedQuestions = this.state.questions.map(q => q.id === question.id ? completedQuestion : q);
     const updatedIndex = this.state.questionIndex + 1;
     const updatedSocialScore = question.type === 'social' ? socialScore + option : socialScore;
     const updatedEconomicsScore = question.type === 'economic' ? economicsScore + option : economicsScore;
@@ -30,8 +28,8 @@ class Compass extends React.Component {
       questions: updatedQuestions,
       questionIndex: updatedIndex,
       socialScore: updatedSocialScore,
-      economicsScore: updatedEconomicsScore,
-    })
+      economicsScore: updatedEconomicsScore
+    });
   }
 
   constructQuestion(question) {
@@ -41,12 +39,12 @@ class Compass extends React.Component {
         <p>{question.content}</p>
         <p>Type: {question.type}</p>
 
-        {question.options.map((option,i)=>{
+        {question.options.map((option, i) => {
           return (
             <div key={i}>
               <button onClick={this.computeChoice.bind(this, option, question)}>{option}</button>
             </div>
-          )
+          );
         })}
       </div>
     );
@@ -58,28 +56,27 @@ class Compass extends React.Component {
       questionIndex: 0,
       socialScore: 0,
       economicsScore: 0
-    })
+    });
   }
 
   render() {
     const { questions, questionIndex, socialScore, economicsScore } = this.state;
-    if(this.state.questions[this.state.questionIndex]){
+    if (this.state.questions[this.state.questionIndex]) {
       const formattedQuestions = this.constructQuestion(questions[questionIndex]);
       return (
         <div>
         {formattedQuestions}
         </div>
       );
-    }else {
-      return (
-        <div>
-          <h1>Game Over</h1>
-          <p>Social: {socialScore}</p>
-          <p>Economic: {economicsScore}</p>
-          <button onClick={this.resetApp}>Reset</button>
-        </div>
-      )
     }
+    return (
+      <div>
+        <h1>Game Over</h1>
+        <p>Social: {socialScore}</p>
+        <p>Economic: {economicsScore}</p>
+        <button onClick={this.resetApp}>Reset</button>
+      </div>
+    );
   }
 }
 
