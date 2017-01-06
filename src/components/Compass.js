c7853f1a94f4725bcb92f11e42a1ca072f660ce3
Evan Gatchell <egatchell@changehealthcare.com>
2017-01-05 22:46:19 -0600

    averages installed.

--- src/components/Compass.js
+++ src/components/Compass.js
@@ -2,78 +2,90 @@ import React from 'react';
 import * as content from '../../content/questions.json';
 
 class Compass extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.constructQuestions = this.constructQuestion.bind(this);
     this.resetApp = this.resetApp.bind(this);
 
-    this.state = {
+    this.state = this.returnOriginalState();
+  }
+
+  returnOriginalState() {
+    return {
       questions: content.questions,
       questionIndex: 0,
       socialScore: 0,
-      economicsScore: 0
-    };
+      socialCount: 0,
+      economicsScore: 0,
+      economicsCount: 0
+    }
   }
 
 
   computeChoice(option, question) {
-    const { socialScore, economicsScore } = this.state;
+    const { socialScore, economicsScore, socialCount, economicsCount } = this.state;
     const completedQuestion = Object.assign(question, { completed: true });
     const updatedQuestions = this.state.questions.map(q => q.id === question.id ? completedQuestion : q);
     const updatedIndex = this.state.questionIndex + 1;
     const updatedSocialScore = question.type === 'social' ? socialScore + option : socialScore;
+    const updatedSocialCount = question.type === 'social' ? socialCount + 1 : socialCount
     const updatedEconomicsScore = question.type === 'economic' ? economicsScore + option : economicsScore;
+    const updatedEconomicsCount = question.type === 'economic' ? economicsCount + 1 : economicsCount;
 
     this.setState({
       questions: updatedQuestions,
       questionIndex: updatedIndex,
       socialScore: updatedSocialScore,
-      economicsScore: updatedEconomicsScore
+      economicsScore: updatedEconomicsScore,
+      socialCount: updatedSocialCount,
+      economicsCount: updatedEconomicsCount
     });
   }
 
+
+
   constructQuestion(question) {
+    // TODO
+    const QuestionOptions = question.options.map((option, i) => {
+      return (
+        <div key={i}>
+          <button onClick={() => this.computeChoice(option, question)}>{option}</button>
+        </div>
+      );
+    });
+
     return (
       <div className="question-block">
         <h3>{question.title}</h3>
         <p>{question.content}</p>
         <p>Type: {question.type}</p>
-
-        {question.options.map((option, i) => {
-          return (
-            <div key={i}>
-              <button onClick={this.computeChoice.bind(this, option, question)}>{option}</button>
-            </div>
-          );
-        })}
+        {QuestionOptions}
       </div>
     );
   }
 
   resetApp() {
-    this.setState({
-      questions: content.questions,
-      questionIndex: 0,
-      socialScore: 0,
-      economicsScore: 0
-    });
+    this.setState(this.returnOriginalState());
   }
 
   render() {
-    const { questions, questionIndex, socialScore, economicsScore } = this.state;
-    if (questions[questionIndex]) {
-      const formattedQuestion = this.constructQuestion(questions[questionIndex]);
+    const { questions, questionIndex, socialScore, socialCount, economicsScore, economicsCount } = this.state;
+    const socialAverage = socialScore / socialCount;
+    const economicsAverage = economicsScore / economicsCount;
+    const currentQuestion = questions[questionIndex];
+    if (currentQuestion) {
+      const formattedQuestion = this.constructQuestion(currentQuestion);
       return (
         <div>
-        {formattedQuestion}
+          {formattedQuestion}
         </div>
       );
     }
     return (
       <div>
         <h1>Game Over</h1>
-        <p>Social: {socialScore}</p>
-        <p>Economic: {economicsScore}</p>
+        <p>Social Score: {socialScore} | Social Count: {socialCount} | X-Axis: {socialAverage}</p>
+        <p>Economics Score: {economicsScore} | Economics Count: {economicsCount} | Y-Axis: {economicsAverage}</p>
         <button onClick={this.resetApp}>Reset</button>
       </div>
     );
